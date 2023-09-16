6ae4c74c2a32f549fcea8abeb598264a302bef61
arisolta <90561629+arisolta@users.noreply.github.com>
2023-09-17 00:22:18 +0200

    Ex 1-7 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, num) {
+    if (num < 0){return "ERROR"}
+    let result = "";
+    let i = 0;
+    while (i < num){
+        result += str;
+        i++;
+    }
+    return result    
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 !== "number"){return "ERROR"};
+    if(num1 < 0 || num2 < 0){return "ERROR"};
+    let min;
+    let max;
+    if(num1 < num2){min = num1; max = num2;}
+    else{min = num2; max = num1;}
+    let result = 0;
+    for (let i = min; i<=max; i++)
+        result += i;
+    return result;
 };
 
 // Do not edit below this line
